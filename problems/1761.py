89008a1bd3723c5fcc6a93a0e13a08d61e830204
Izabella Alves <iap21010705@gmail.com>
2024-11-02 12:06:01 -0300

    feat: adiciona criacao da matriz de adjacencias do problema 1761

--- problems/1761.py
+++ problems/1761.py
@@ -1,2 +1,8 @@
 class Solution:
     def minTrioDegree(self, n: int, edges: List[List[int]]) -> int:
+        
+        matriz = []
+        
+        for i in range(n):
+            linha = [False] * n
+            matriz.append(linha)

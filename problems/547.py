a54e19f88a528ebf711b4c47262b2edfb0b800c6
Izabella Alves <iap21010705@gmail.com>
2024-11-02 10:45:27 -0300

    feat: adiciona busca em profundidade na questao 547

--- problems/547.py
+++ problems/547.py
@@ -1,6 +1,10 @@
 class Solution(object):
     def findCircleNum(self, isConnected):
-        """
-        :type isConnected: List[List[int]]
-        :rtype: int
-        """
+        n = len(isConnected)
+        visitado = [False] * n
+        
+        def dfs(cidade):
+            visitado[cidade] = True
+            for vizinha in range(n):
+                if isConnected[cidade][vizinha] == 1 and not visitado[vizinha]:
+                    dfs(vizinha)

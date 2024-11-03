4133640dfc237f151ec2cb740bb8f2a8859a271a
GZaranza <gabrielzaranza@hotmail.com>
2024-11-03 11:04:24 -0300

    adicionando a parte do contador de cores

--- problems/1857.py
+++ problems/1857.py
@@ -1,18 +1,29 @@
-
+import pprint
 def largestPathValue(colors: str, edges: list[list[int]]) -> int:
+    
+    colors_cont = {}
+    
+    for i in colors:
+        if i not in colors_cont:
+            colors_cont[i] = 0
+        
     
     graph = {}
     
     for item in edges:
         if item[0] not in graph:
+            
             graph[item[0]] = {
                 'vizinhos': [],
-                'visitado': False
+                'visitado': False,
+                'cor': colors[item[0]]
             }
         if item[1] not in graph:
+            
             graph[item[1]] = {
                 'vizinhos': [],
-                'visitado': False
+                'visitado': False,
+                'cor': colors[item[1]]
             }
         
         graph[item[0]]['vizinhos'].append(item[1])
@@ -29,20 +40,21 @@ def largestPathValue(colors: str, edges: list[list[int]]) -> int:
     
     for vertex in graph:
         if graph[vertex]['visitado'] == False:
-            print(f"visitando {vertex}")
+        
             enqueue(queue,vertex)
             graph[vertex]['visitado'] = True
+            colors_cont[graph[vertex]['cor']]+=1
             
             while len(queue) > 0:
                 u = dequeue(queue)
                 
-                for v in graph[u]['vizinhos']:
+                for v in graph[u]['vizinhos']: 
                     if graph[v]['visitado'] == False:
-                        print(f"visitando {v}")
                         graph[v]['visitado'] = True
+                        colors_cont[graph[v]['cor']]+=1
                         enqueue(queue,v)
-            
-        
+         
+    print(max(colors_cont, key=colors_cont.get))
     
 
 largestPathValue("abaca",[[0,1],[0,2],[2,3],[3,4]])        
